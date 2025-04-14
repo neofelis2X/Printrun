6d56e7977efd74dfc2a558bc6cf6650dc089c08b
neofelis2X <m.kneidinger@mailbox.org>
2025-04-15 00:39:43 +0200

    ogl: load binary stl models with plater, wip

--- printrun/assets/shader/basic.vert.glsl
+++ printrun/assets/shader/basic.vert.glsl
@@ -2,12 +2,16 @@
 layout (location = 0) in vec3 vPos;
 layout (location=1) in vec4 vColor;
 layout (location=2) in vec3 vNormal;
+
 uniform mat4 modelViewProjection;
+
 out vec4 fragmentColor;
+out vec3 normal;
 
 void main()
 {
         gl_Position = modelViewProjection * vec4(vPos.x, vPos.y, vPos.z, 1.0);
         fragmentColor = vColor;
+        normal = vNormal;
 }
 
