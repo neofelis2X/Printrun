b9482d512f72c76e95fc96628de903cdfc45d1be
neofelis2X <m.kneidinger@mailbox.org>
2026-06-19 22:27:45 +0200

    ogl: move renderer functions around

--- printrun/gl/renderer.py
+++ printrun/gl/renderer.py
@@ -79,6 +79,7 @@ class MapBufferRange:
         return False
 
 
+#### SHADER ####
 def load_shader() -> Optional[Dict[str, shader.ShaderProgram]]:
     if not SRC_SHADER_DIR.is_dir():
         logging.error("GL: Directory containing the \
@@ -142,27 +143,8 @@ def _compile_shader(src: Path, kind: shader.ShaderType) -> Optional[shader.Shade
 
     return new_shader
 
-def load_uniform(shader_id, uniform_name: str, data):
-    location = glGetUniformLocation(shader_id, uniform_name.encode())
-    if location == -1:
-        logging.warning("GL: Could not find Uniform location: %s" % uniform_name)
-        return
 
-    if isinstance(data, bool):
-        glUniform1i(location, int(data))
-    elif isinstance(data, float):
-        glUniform1f(location, data)
-    elif uniform_name == "u_oColor":
-        glUniform4f(location, *data)
-    elif uniform_name == "viewPos":
-        glUniform3f(location, *data.data)  # FIXME: looks strange
-    elif uniform_name == "u_NormalMat":
-        ptr = data.ctypes.data_as(ctypes.POINTER(GLfloat))
-        glUniformMatrix3fv(location, 1, GL_FALSE, ptr)
-    elif uniform_name == "modelMat":
-        ptr = data.ctypes.data_as(ctypes.POINTER(GLfloat))
-        glUniformMatrix4fv(location, 1, GL_FALSE, ptr)
-
+#### BUFFER AND BUFFER DATA ####
 def get_normal_mat(model_mat: np.ndarray) -> np.ndarray:
     sub = model_mat[0:3, 0:3]
     try:
@@ -196,72 +178,6 @@ def interleave_vertex_data(verts, color, normal: Optional[np.ndarray]=None,
                 data[iv + 7:iv + 10] = normal
 
     return data
-
-def create_ubo():
-    """
-    Creates a uniform buffer object
-    """
-    ubo = GLuint(0)
-    bytesize = ctypes.sizeof(GLfloat)
-    glGenBuffers(1, ubo)
-    glBindBuffer(GL_UNIFORM_BUFFER, ubo)
-    glBufferData(GL_UNIFORM_BUFFER, GLsizeiptr(256 * bytesize), None, GL_DYNAMIC_DRAW)
-    glBindBuffer(GL_UNIFORM_BUFFER, 0)
-
-    glBindBufferRange(GL_UNIFORM_BUFFER, 0, ubo, GLintptr(0), GLsizeiptr(256 * bytesize))
-    #glBindBufferRange(GL_UNIFORM_BUFFER, 1, ubo, 64 * bytesize, 8 * bytesize)
-
-    return ubo
-
-def bind_shader_ublock(shaderlist, ublock_name: str) -> None:
-    ublock_index = GLuint(0)
-    binding_point = 0
-    byte_name = ublock_name.encode(encoding="utf-8")
-    for sh in shaderlist.values():
-        ublock_index = glGetUniformBlockIndex(sh.id, byte_name)
-        glUniformBlockBinding(sh.id, ublock_index, binding_point)
-
-def update_ubo_view(ubo, camera):
-    glBindBuffer(GL_UNIFORM_BUFFER, ubo)
-
-    mat = camera.projection @ camera.view
-    data = mat.flatten(order='F')
-    offset = 0
-    eye = camera.eye
-    eye_offset = offset + 2 * data.nbytes
-    glBufferSubData(GL_UNIFORM_BUFFER, GLintptr(offset), data.nbytes, data.ctypes.data)
-    glBufferSubData(GL_UNIFORM_BUFFER, eye_offset, eye.nbytes,
-                    eye.ctypes.data)
-
-    glBindBuffer(GL_UNIFORM_BUFFER, 0)
-
-def update_ubo_viewport(ubo, camera, viewport):
-    glBindBuffer(GL_UNIFORM_BUFFER, ubo)
-    bytesize = ctypes.sizeof(GLfloat)
-
-    mat = camera.projection2d
-    offset = mat.nbytes
-    vp = np.array(viewport, dtype=np.float32)
-    vp_offset = offset + (16 + 4) * bytesize
-    glBufferSubData(GL_UNIFORM_BUFFER, offset, mat.nbytes, mat.ctypes.data)
-    glBufferSubData(GL_UNIFORM_BUFFER, vp_offset, GLintptr(vp.nbytes), vp.ctypes.data)
-
-    glBindBuffer(GL_UNIFORM_BUFFER, 0)
-
-def update_ubo_transform(ubo, transform_mat):
-    glBindBuffer(GL_UNIFORM_BUFFER, ubo)
-    bytesize = ctypes.sizeof(GLfloat)
-
-    tm = transform_mat
-    offset = (16 + 16 + 4 + 4) * bytesize
-    nm = get_normal_mat(tm)
-    # A 3x3 matrix has a 4x3 layout in std140, hence the padding
-    nm_padded = np.pad(nm.T, ((0, 0), (0, 1)), mode="constant")
-
-    data = np.concatenate((tm, nm_padded))
-    glBufferSubData(GL_UNIFORM_BUFFER, GLintptr(offset), GLsizeiptr(data.nbytes), data.ctypes.data)
-
-    glBindBuffer(GL_UNIFORM_BUFFER, 0)
 
 def create_buffers(create_ebo=True, lines_only=False):
     """
@@ -338,4 +254,95 @@ def get_gl_array(pylist):
 
     array_type = casttype * len(pylist)
     return array_type(*pylist)
+
+
+### UNIFORMS ####
+def load_uniform(shader_id, uniform_name: str, data):
+    location = glGetUniformLocation(shader_id, uniform_name.encode())
+    if location == -1:
+        logging.warning("GL: Could not find Uniform location: %s" % uniform_name)
+        return
+
+    if isinstance(data, bool):
+        glUniform1i(location, int(data))
+    elif isinstance(data, float):
+        glUniform1f(location, data)
+    elif uniform_name == "u_oColor":
+        glUniform4f(location, *data)
+    elif uniform_name == "viewPos":
+        glUniform3f(location, *data.data)  # FIXME: looks strange
+    elif uniform_name == "u_NormalMat":
+        ptr = data.ctypes.data_as(ctypes.POINTER(GLfloat))
+        glUniformMatrix3fv(location, 1, GL_FALSE, ptr)
+    elif uniform_name == "modelMat":
+        ptr = data.ctypes.data_as(ctypes.POINTER(GLfloat))
+        glUniformMatrix4fv(location, 1, GL_FALSE, ptr)
+
+
+#### UNIFORM BUFFER OJECTS ####
+def create_ubo():
+    """
+    Creates a uniform buffer object
+    """
+    ubo = GLuint(0)
+    bytesize = ctypes.sizeof(GLfloat)
+    glGenBuffers(1, ubo)
+    glBindBuffer(GL_UNIFORM_BUFFER, ubo)
+    glBufferData(GL_UNIFORM_BUFFER, GLsizeiptr(256 * bytesize), None, GL_DYNAMIC_DRAW)
+    glBindBuffer(GL_UNIFORM_BUFFER, 0)
+
+    glBindBufferRange(GL_UNIFORM_BUFFER, 0, ubo, GLintptr(0), GLsizeiptr(256 * bytesize))
+    #glBindBufferRange(GL_UNIFORM_BUFFER, 1, ubo, 64 * bytesize, 8 * bytesize)
+
+    return ubo
+
+def bind_shader_ublock(shaderlist, ublock_name: str) -> None:
+    ublock_index = GLuint(0)
+    binding_point = 0
+    byte_name = ublock_name.encode(encoding="utf-8")
+    for sh in shaderlist.values():
+        ublock_index = glGetUniformBlockIndex(sh.id, byte_name)
+        glUniformBlockBinding(sh.id, ublock_index, binding_point)
+
+def update_ubo_view(ubo, camera):
+    glBindBuffer(GL_UNIFORM_BUFFER, ubo)
+
+    mat = camera.projection @ camera.view
+    data = mat.flatten(order='F')
+    offset = 0
+    eye = camera.eye
+    eye_offset = offset + 2 * data.nbytes
+    glBufferSubData(GL_UNIFORM_BUFFER, GLintptr(offset), data.nbytes, data.ctypes.data)
+    glBufferSubData(GL_UNIFORM_BUFFER, eye_offset, eye.nbytes,
+                    eye.ctypes.data)
+
+    glBindBuffer(GL_UNIFORM_BUFFER, 0)
+
+def update_ubo_viewport(ubo, camera, viewport):
+    glBindBuffer(GL_UNIFORM_BUFFER, ubo)
+    bytesize = ctypes.sizeof(GLfloat)
+
+    mat = camera.projection2d
+    offset = mat.nbytes
+    vp = np.array(viewport, dtype=np.float32)
+    vp_offset = offset + (16 + 4) * bytesize
+    glBufferSubData(GL_UNIFORM_BUFFER, offset, mat.nbytes, mat.ctypes.data)
+    glBufferSubData(GL_UNIFORM_BUFFER, vp_offset, GLintptr(vp.nbytes), vp.ctypes.data)
+
+    glBindBuffer(GL_UNIFORM_BUFFER, 0)
+
+def update_ubo_transform(ubo, transform_mat):
+    glBindBuffer(GL_UNIFORM_BUFFER, ubo)
+    bytesize = ctypes.sizeof(GLfloat)
+
+    tm = transform_mat
+    offset = (16 + 16 + 4 + 4) * bytesize
+    nm = get_normal_mat(tm)
+    # A 3x3 matrix has a 4x3 layout in std140, hence the padding
+    nm_padded = np.pad(nm.T, ((0, 0), (0, 1)), mode="constant")
+
+    data = np.concatenate((tm, nm_padded))
+    glBufferSubData(GL_UNIFORM_BUFFER, GLintptr(offset), GLsizeiptr(data.nbytes), data.ctypes.data)
+
+    glBindBuffer(GL_UNIFORM_BUFFER, 0)
 
